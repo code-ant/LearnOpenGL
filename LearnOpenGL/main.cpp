d71ff6f5a803947314cad85757477aeb61fde915
pengtai.wang <2358326343@qq.com>
2022-06-20 16:05:49 +0800

    使用两个着色器，两个三角形一个绘制黄色，一个绘制橙色

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -16,12 +16,19 @@ const char *vertexShaderSource = "#version 330 core \n"
     "{ \n"
     "   gl_Position = vec4(aPos, 1.0); \n"
     "} \n\0";
-const char *fragmentShaderSource = "#version 330 core \n"
+const char *fragmentShader1Source = "#version 330 core \n"
     "out vec4 FragColor; \n"
     "void main() \n"
     "{ \n"
     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
     "}\n\0";
+const char *fragmentShader2Source = "#version 330 core \n"
+    "out vec4 FragColor; \n"
+    "void main() \n"
+    "{ \n"
+    " FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f); \n"
+    "} \n\0";
+
 
 int main()
 {
@@ -61,42 +68,26 @@ int main()
     // --
     // 顶点着色器
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int fragmentShaderOrange = glCreateShader(GL_FRAGMENT_SHADER);
+    unsigned int fragmentShaderYellow = glCreateShader(GL_FRAGMENT_SHADER);
+    unsigned int shaderProgramOrange = glCreateProgram();
+    unsigned int shaderProgramYellow = glCreateProgram();
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
-    // 是否编译报错
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPLIE_FAILED \n" << infoLog << std::endl;
-    }
-    // 片段着色器
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // 编译结果
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::FRAGMENT::COMPILATION_FAILD \n" << infoLog << std::endl;
-    }
-    // 链接着色器到着色程序
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // 链接结果
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED \n" << infoLog << std::endl;
-    }
-    // 删除着色器
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glShaderSource(fragmentShaderOrange, 1, &fragmentShader1Source, NULL);
+    glCompileShader(fragmentShaderOrange);
+    glShaderSource(fragmentShaderYellow, 1, &fragmentShader2Source, NULL);
+    glCompileShader(fragmentShaderYellow);
+    // 链接着色器到第一个着色程序
+    glAttachShader(shaderProgramOrange, vertexShader);
+    glAttachShader(shaderProgramOrange, fragmentShaderOrange);
+    glLinkProgram(shaderProgramOrange);
+    
+    // 链接第二个程序
+    glAttachShader(shaderProgramYellow, vertexShader);
+    glAttachShader(shaderProgramYellow, fragmentShaderYellow);
+    glLinkProgram(shaderProgramYellow);
+
     // 顶点数据和属性
     // --
     float firstTriangle[] = {
@@ -131,7 +122,7 @@ int main()
     // 无论如何，修改其他VAO都需要调用glBindVertexArray，因此通常不会在没有直接必要时解除绑定VAO
     glBindVertexArray(0);
     // 画线
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     
     // 绘制
     // --
@@ -146,11 +137,12 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        glUseProgram(shaderProgram);
+        glUseProgram(shaderProgramOrange);
         // 使用VAO[0]画第一个三角形
         glBindVertexArray(VAOs[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // 使用VAO[1]画第二个
+        glUseProgram(shaderProgramYellow);
         glBindVertexArray(VAOs[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         
@@ -164,7 +156,8 @@ int main()
     // 释放内存属于非必须操作
     glDeleteVertexArrays(2, VAOs);
     glDeleteBuffers(2, VBOs);
-    glDeleteProgram(shaderProgram);
+    glDeleteProgram(shaderProgramOrange);
+    glDeleteProgram(shaderProgramYellow);
     
     // 终止运行
     // 释放GLFW资源
