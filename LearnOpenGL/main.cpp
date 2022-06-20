70240f77cb8aa56ce9062a583ee6e96c74362a09
pengtai.wang <2358326343@qq.com>
2022-06-20 14:42:14 +0800

    添加更多顶点到数据中，使用glDrawArrays，绘制两个彼此相连的三角形

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -100,27 +100,25 @@ int main()
     // 顶点数据和属性
     // --
     float vertices[] = {
-        0.5f,  0.5f,  0.0f,
-        0.5f, -0.5f,  0.0f,
-       -0.5f, -0.5f,  0.0f,
-       -0.5f,  0.5f,  0.0f
+        0.5f,  0.0f,  0.0f,
+        0.0f,  0.5f,  0.0f,
+        0.0f,  0.0f,  0.0f,
+        
+       -0.5f,  0.0f,  0.0f,
+        0.0f, -0.5f,  0.0f,
+        0.0f,  0.0f,  0.0f
     };
-    unsigned int indices[] = {
-        0, 1, 3,
-        1, 2, 3
-    };
-    unsigned int VBO, VAO, EBO;
+
+    
+    
+    unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
     // 绑定顶点数组对象，然后绑定顶点缓冲区，设置顶点属性
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -155,8 +153,8 @@ int main()
         // 第一个三角形
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);// 非必须，不需要每次都解绑
         
         // 交换颜色缓冲区
@@ -169,7 +167,6 @@ int main()
     // 释放内存属于非必须操作
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
     
     // 终止运行
