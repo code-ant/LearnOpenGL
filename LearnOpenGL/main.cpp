550b5c4fd538644cd9c3a61b2ffd3dbb23c9908d
pengtai.wang <2358326343@qq.com>
2022-07-01 17:57:00 +0800

    在顶点中添加颜色

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -2,7 +2,6 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include <cmath>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -13,17 +12,20 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core \n"
     "layout (location = 0) in vec3 aPos; \n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 myColor;\n"
     "void main() \n"
     "{ \n"
     "   gl_Position = vec4(aPos, 1.0); \n"
+    "   myColor = aColor;\n"
     "} \n\0";
 
 const char *fragmentShader1Source = "#version 330 core \n"
     "out vec4 FragColor; \n"
-    "uniform vec4 myColor;\n"
+    "in vec3 myColor;\n"
     "void main() \n"
     "{ \n"
-    "   FragColor = myColor; \n"
+    "   FragColor = vec4(myColor, 1.0); \n"
     "}\n\0";
 
 int main()
@@ -102,9 +104,9 @@ int main()
     // 顶点数据和属性
     // --
     float firstTriangle[] = {
-        -0.9f, -0.5f, 0.0f,  // left
-        -0.0f, -0.5f, 0.0f,  // right
-        -0.45f, 0.5f, 0.0f,  // top
+        -0.9f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // left
+        -0.0f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // right
+        -0.45f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top
     };
 
     unsigned int VBO, VAO;
@@ -115,8 +117,12 @@ int main()
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // 位置信息
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // 颜色信息
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     // glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 
@@ -146,10 +152,10 @@ int main()
         glUseProgram(shaderProgram);
         
         // 更新uniform值
-        double time = glfwGetTime();
-        float greenValue = static_cast<float>(sin(time) / 2.0 + 0.5);
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "myColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        // double time = glfwGetTime();
+        //float greenValue = static_cast<float>(sin(time) / 2.0 + 0.5);
+        //int vertexColorLocation = glGetUniformLocation(shaderProgram, "myColor");
+        //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         
         // 画一个三角形
         glDrawArrays(GL_TRIANGLES, 0, 3);
